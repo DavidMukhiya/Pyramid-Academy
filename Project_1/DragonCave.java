f04e6fb48940362ea3308e978e5fa94d29bce7cf
David Prakash Mukhiya <davidmukhiya09@gmail.com>
2022-07-15 14:58:21 -0700

    added comment and minor changes.

--- Project_1/DragonCave.java
+++ Project_1/DragonCave.java
@@ -3,17 +3,21 @@ package Project_1;
 import java.util.Scanner;
 
 public class DragonCave {
+    //cave Method with take caveNum Input from user.
     public void cave(int caveNum) {
         switch (caveNum) {
+            //for cave 1
             case 1:
-                System.out.println("you Approach the cave..\n" +
+                System.out.println("\nyou Approach the cave..\n" +
                         "It is dark and spooky..\n" +
                         "A large dragon jumps out in front of you!\n" +
                         "He opens his jaws and...\n" +
                         "Gobbles you down in one bite!");
                 break;
+
+            //for cave 2
             case 2:
-                System.out.println("you Approach the cave..\n" +
+                System.out.println("\nyou Approach the cave..\n" +
                         "It is warm and happy..\n" +
                         "the dragon is friendly..\n" +
                         "will share his treasure with you");
@@ -21,6 +25,8 @@ public class DragonCave {
         };
     };
     public static void main(String[] args) {
+
+        //Intro
         System.out.println("\nYou are in a Land full of Dragon.\n" +
                 "In front you, you see two cave.\n" +
                 "In one cave, the dragon is greedy\n" +
@@ -32,16 +38,22 @@ public class DragonCave {
                 "Enter No '2' for Cave Two\n");
         System.out.print("Enter the No: ");
 
+        //Taking Input from user
         Scanner sc = new Scanner(System.in);
         int caveNum = sc.nextInt();
         System.out.print("You Enter the No: " + caveNum + "\n");
+
+        //checking the number for cave
         if (caveNum == 1 || caveNum == 2) {
+            //creating the DragonCave object and calling cave method.
             DragonCave dragonCave = new DragonCave();
             dragonCave.cave(caveNum);
         } else {
+            //if user input anything other than 1 or 2.
             System.out.print("\nPlease Enter No 1 or 2: ");
             caveNum = sc.nextInt();
             System.out.print("You Enter the No: " + caveNum);
+            //creating the DragonCave object and calling cave method.
             DragonCave dragonCave = new DragonCave();
             dragonCave.cave(caveNum);
         }
